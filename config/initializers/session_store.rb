888d6167c88a61c42032ca93524347d1ff63c0df
Zeeshan Malik <zeeshan.m.malik@gmail.com>
2015-05-17 17:42:32 -0400

    renaming app to bootstrap-flatlab-theme

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_actistaff-www_session'
+Rails.application.config.session_store :cookie_store, key: '_boostrap-flatlab-theme_session'
